7a1c49387ce9fe112b6f622d7494a6100c7787d6
Àngel Díaz <angeldm@gmail.com>
2019-03-22 21:53:00 +0100

    first commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'angeldm_es_es', __DIR__);
